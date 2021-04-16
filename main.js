a88d6e8c1a04077eecbab867ae33454c19ffad74
Ankita Ganguly <68239797+iamkita@users.noreply.github.com>
2021-04-16 12:24:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function direct_qp() {
+    window.location = "q-physics.html";
+}
+
+function direct_qc() {
+    window.location = "q-computing.html";
+}
+
+function direct_qm() {
+    window.location = "q-mechanics.html";
+}
---
+++ q-mechanics.js
@@ -0,0 +1,3 @@
+function funcHome() {
+    window.location = "index.html";
+}
